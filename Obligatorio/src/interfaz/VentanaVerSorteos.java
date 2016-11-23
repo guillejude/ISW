eaf9db46772a239dbd2c9fc583b4a50238fb14a3
Rafael Alonso <pepe1615@hotmail.com>
2016-11-23 15:06:00 -0300

    Se modificó la ventana de ver sorteos para que en la tabla se vea según el restaurante elegido.

--- Obligatorio/src/interfaz/VentanaVerSorteos.java
+++ Obligatorio/src/interfaz/VentanaVerSorteos.java
@@ -12,6 +12,7 @@ import dominio.Sorteo;
 import java.awt.Color;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import static javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE;
@@ -32,25 +33,12 @@ public class VentanaVerSorteos extends javax.swing.JFrame {
         this.padre = ventanaPrincipal;
         this.sistema = sistema;
         DefaultTableModel table = (DefaultTableModel) tblSorteos.getModel();
-        DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-        table.setRowCount(this.sistema.getCantSorteos());
-        int k = 0;
-        for(int i = 0; i<this.sistema.getRestaurantes().size(); i++){
-            Restaurante restaurante = this.sistema.getRestaurantes().get(i);
-            for(int j = 0; j<restaurante.getSorteos().size(); j++){
-                Sorteo elSorteo = (Sorteo) restaurante.getSorteos().get(j);
-                tblSorteos.setValueAt(elSorteo.getNombre(), k, 0);
-                tblSorteos.setValueAt(df.format(elSorteo.getFechaInicio()), k, 1);
-                tblSorteos.setValueAt(df.format(elSorteo.getFechaInicio()), k, 2);
-                tblSorteos.setValueAt(elSorteo.getCantidadGanadores(), k, 3);
-                String realizado = "NO";
-                if(elSorteo.isRealizado()){
-                    realizado = "SI";
-                }
-                tblSorteos.setValueAt(realizado, k, 4);
-                k++;
-            }
+        Restaurante[] restaurantes = new Restaurante[this.sistema.getRestaurantes().size()];
+        for(int i = 0; i<restaurantes.length; i++){
+            restaurantes[i] = this.sistema.getRestaurantes().get(i);
         }
+        cmbBxRestaurantes.setModel(new DefaultComboBoxModel(restaurantes));
+        
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
     }
 
@@ -66,6 +54,7 @@ public class VentanaVerSorteos extends javax.swing.JFrame {
         lblTitulo = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblSorteos = new javax.swing.JTable();
+        cmbBxRestaurantes = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
@@ -88,7 +77,7 @@ public class VentanaVerSorteos extends javax.swing.JFrame {
                 {null, null, null, null, null}
             },
             new String [] {
-                "Nombre", "Inicio", "Fin", "Cant. Ganadores", "Realizado"
+                "Nombre", "Inicio", "Fin", "Premios", "Realizado"
             }
         ) {
             Class[] types = new Class [] {
@@ -101,18 +90,31 @@ public class VentanaVerSorteos extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(tblSorteos);
 
+        cmbBxRestaurantes.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cmbBxRestaurantes.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                cmbBxRestaurantesFocusLost(evt);
+            }
+        });
+        cmbBxRestaurantes.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmbBxRestaurantesActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 482, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
             .addGroup(layout.createSequentialGroup()
+                .addGap(189, 189, 189)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 482, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(201, 201, 201)
-                        .addComponent(lblTitulo)))
+                    .addComponent(cmbBxRestaurantes, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblTitulo))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -120,9 +122,11 @@ public class VentanaVerSorteos extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(lblTitulo)
-                .addGap(20, 20, 20)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 247, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(37, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(cmbBxRestaurantes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 249, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -141,8 +145,33 @@ public class VentanaVerSorteos extends javax.swing.JFrame {
         this.padre.setVisible(true);
     }//GEN-LAST:event_formWindowClosing
 
+    private void cmbBxRestaurantesFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_cmbBxRestaurantesFocusLost
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cmbBxRestaurantesFocusLost
+
+    private void cmbBxRestaurantesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmbBxRestaurantesActionPerformed
+        // TODO add your handling code here:
+        Restaurante restaurante =(Restaurante) cmbBxRestaurantes.getSelectedItem();
+        DefaultTableModel table = (DefaultTableModel) tblSorteos.getModel();
+        DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+        table.setRowCount(restaurante.getSorteos().size());
+        for(int j = 0; j<restaurante.getSorteos().size(); j++){
+                Sorteo elSorteo = (Sorteo) restaurante.getSorteos().get(j);
+                tblSorteos.setValueAt(elSorteo.getNombre(), j, 0);
+                tblSorteos.setValueAt(df.format(elSorteo.getFechaInicio()), j, 1);
+                tblSorteos.setValueAt(df.format(elSorteo.getFechaInicio()), j, 2);
+                tblSorteos.setValueAt(elSorteo.getCantidadGanadores(), j, 3);
+                String realizado = "NO";
+                if(elSorteo.isRealizado()){
+                    realizado = "SI";
+                }
+                tblSorteos.setValueAt(realizado, j, 4);
+            }
+    }//GEN-LAST:event_cmbBxRestaurantesActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> cmbBxRestaurantes;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblTitulo;
     private javax.swing.JTable tblSorteos;
